34fc8130491e12f139c4d83673c41e2f5a8622b6
viperjke <viper_jke@mail.ru>
2017-03-12 21:47:45 +0200

    Revert "SS-28: UI for Preview page - Determined general color scheme - Added UI for preview page - Added general style for buttons (default) - Created general structure folder for ui project - Added auth duck to authorization module"
    This reverts commit 4851d9af9d8379a5647443817f5fb41b3f4f4802.

--- SmartScheduler.Site/src/components/header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HeaderComponent = (props) => (
-    <div className="l-header">
-        Header
-    </div>
-);
-
-export default HeaderComponent;
--- SmartScheduler.Site/src/modules/auth/index.js
+++
@@ -1,1 +0,0 @@
-export { actionCreators, actionTypes, default as reducer } from './duck';
