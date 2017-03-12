144dc645b2e20404ef1e93e777795cc5d9878b27
viperjke <viper_jke@mail.ru>
2017-03-12 21:47:45 +0200

    Revert "SS-26 Create and setup git repository Create 3 folders Docs, Site, C# project For C# solution create 4 projects: Models, Models.Test, Services, Services.Test It default initial empty project. Web.API project has default examples Authentication and Value controllers. Look at this examples to undestand as we must change it for our project."
    This reverts commit b09bfc0a9fb8f6a6e444cd1b47490e272a8f248a.

--- SmartScheduler/SmartScheduler.Services/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace SmartScheduler.Services.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- SmartScheduler/SmartScheduler.Services/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartScheduler.Services.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
